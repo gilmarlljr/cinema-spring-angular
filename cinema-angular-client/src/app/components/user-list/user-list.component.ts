5bf10c2fb4a48041faa448aba7a04f5e5b68700c
Gilmar <gilmarlljr@hotmail.com>
2020-03-29 17:19:11 -0300

    sla

---
+++ cinema-angular-client/src/app/components/user-list/user-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {UserService} from "../../shared/cinema/user.service";
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+  users: Array<any>
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.userService.getAll().subscribe((response)=>{
+      this.users = response.data;
+    })
+  }
+
+}
---
+++ client-teste/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CarListComponent } from './car-list/car-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
